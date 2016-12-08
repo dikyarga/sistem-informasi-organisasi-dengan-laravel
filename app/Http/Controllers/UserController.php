9b69c8cb85385e927e709006ee9996e9c4731006
nurul irfan <irfanlongstride@gmail.com>
2016-12-08 08:53:49 +0700

    fix post list in user profile not shown

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -70,9 +70,9 @@ class UserController extends Controller {
     }
     $data['comments_count'] = $data['user'] -> comments -> count();
     $data['posts_count'] = $data['user'] -> posts -> count();
-    $data['posts_active_count'] = $data['user'] -> posts -> where('active', '1') -> count();
+    $data['posts_active_count'] = $data['user'] -> posts -> where('active', 1) -> count();
     $data['posts_draft_count'] = $data['posts_count'] - $data['posts_active_count'];
-    $data['latest_posts'] = $data['user'] -> posts -> where('active', '1') -> take(5);
+    $data['latest_posts'] = $data['user'] -> posts -> where('active', 1) -> take(5);
     $data['latest_comments'] = $data['user'] -> comments -> take(5);
     return view('admin.profile', $data);
   }
